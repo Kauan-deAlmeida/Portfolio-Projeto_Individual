751432f24d81cda080371bfa0b73e9b9dba7b3c8
Kauan-deAlmeida <kauan.almeida@sptech.school>
2023-11-15 03:32:11 -0300

    Final da pagina com conteudo de 'Sobre mim', implementado fotos e texto

--- public/main.js
+++ public/main.js
@@ -1,7 +1,21 @@
 var navBar = document.querySelector(".header");
 
-function mudarCor(){
+function mudarCor() {
     navBar.classList.toggle("active", scrollY);
 }
 
 window.addEventListener("scroll", mudarCor)
+
+
+var cartoon = document.querySelector(".cartoonImg img");
+var describe = document.querySelectorAll(".boxDescribe");
+var zoomCartoon = document.getElementById("cartoonImgZoom");
+
+function espandirImagemDescribe() {
+    zoomCartoon.classList.add("active");
+}
+
+function removerImagemDescribe() {
+    zoomCartoon.classList.remove("active");
+}
+
