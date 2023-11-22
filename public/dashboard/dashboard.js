ed12b8d05c427781995c4191faa00cf8f64ed71f
Kauan-deAlmeida <kauan.almeida@sptech.school>
2023-11-21 21:02:13 -0300

    Atualizado ordem nas fotos da dashboard

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -5,46 +5,46 @@ function abrirAula(card){
     if(idVideo == 1){
         video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 2){
         video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 3){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 5){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 6){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 7){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 8){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 9){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 10){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 11){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 12){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 13){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 14){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 15){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-    else if(idVideo == 1){
+    else if(idVideo == 16){
             video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else{
