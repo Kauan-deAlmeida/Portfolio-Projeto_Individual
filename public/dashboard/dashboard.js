bc6ad6e8ab056a5c8cfb3d22e71a8f6b5ce777bd
Kauan-deAlmeida <kauan.almeida@sptech.school>
2023-11-21 23:53:47 -0300

    Conectado videos aulas do curso

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -3,55 +3,52 @@ function abrirAula(card){
     var video = "";
 
     if(idVideo == 1){
-        video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/4cXb-JYLPXA" title="How I Practice Drawing the Body" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 2){
-        video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/X3P7_VlAMIc" title="How to draw Bodies / Tutorial and Practice ✍✍" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 3){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/vci4UVdTLJk" title="Study Human Drawing" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+    }
+    else if(idVideo == 4){
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/G7LAROdJ8I4" title="How to Draw a Body (Clothed) for Beginners (Tutorial)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 5){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/Jx7uqgIExTc" title="How to Draw Body with Simple Anatomy /Drawing Class for Beginners / Figure Drawing" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 6){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/WZBhkLyVtTM" title="How to Draw a Girl 3/4 Angle (001) / Face Practice" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 7){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/h7CwyvlQfH0" title="How to Draw an Attractive Boy (002) / Face Practice" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 8){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/tn_7opSs9vk" title="How to Draw a Girl with an Attractive Angle (003) / Face Practice" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 9){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/FXNcH6EIG9s" title="Como Desenhar uma Garota em Ângulo Baixo (012) / Prática de Desenho de Rosto" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 10){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hayrR-Xf7V8" title="How to draw face - Face Proportions Drawing" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 11){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/eO5-rd5TNig" title="iPad Watercolor Painting with Procreate / Custom Brush / How to paint a girl" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 12){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/eAhHgPZASYQ" title="How to draw Hands / Useful Tips!! / Tutorials (Easy way) / (Part 1)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 13){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/eabPsiWaimE" title="How to draw Hands / Useful Tips!! / Tutorials (Part 2)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 14){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/iMuxgdCO2gM" title="Como desenhar mãos / Dicas úteis!!! / Tutoriais (Parte 3)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else if(idVideo == 15){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
-    }
-    else if(idVideo == 16){
-            video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/9rkYCG2jQvA" title="How to draw legs / Tutorial &amp; Practice (Sitting pose)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
     else{
-        video = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        boxes.innerHTML = `<iframe width="887" height="499" src="https://www.youtube.com/embed/hf9THaviPvM" title="Como desenhar pés / Dicas úteis e tutoriais !!!" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
     }
-
-    
-    containerVideo.innerHTML = video ;
 
 }
