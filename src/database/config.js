921a5abc6815170f5e77845fa14a383cc7c60370
Kauan-deAlmeida <kauan.almeida@sptech.school>
2023-11-22 21:58:53 -0300

    Adicionado função para passar parâmetros para banco de dados atraves do config.js

--- src/database/config.js
+++ src/database/config.js
@@ -1,2 +1,71 @@
 var mysql = require("mysql2");
+//  var sql = require('mssql');
 
+//  CONEXÃO DO SQL SERVER - AZURE (NUVEM)
+var sqlServerConfig = {
+    server: "SEU_SERVIDOR",
+    database: "SEU_BANCO_DE_DADOS",
+    user: "SEU_USUARIO",
+    password: "SUA_SENHA",
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: true, // for azure
+    }
+}
+
+// CONEXÃO DO MYSQL WORKBENCH
+var mysqlConfig = {
+    host: "localhost",
+    database: "TeamArt",
+    user: "root",
+    password: "110720",
+};
+
+function executar(instrucao){
+    // VERIFICA A VARIÁVEL DE AMBIENTE SETADA EM app.js
+    if (ProcessingInstruction.env.AMBIENTE_PROCESSO == "producao"){
+        return new Promise(function (resolve, reject){
+            sql.connect(sqlServerConfig).then(function () {
+                return sql.query(instrucao);
+            }).then(function (resultados){
+                console.log(resulrtados);
+                resolve(resultados.recordset);
+            }).catch(function (erro) {
+                reject(erro);
+                console.log('Erro: ', erro);
+            });
+            sql.on('error', function (erro){
+                return ("ERRO NO SQL SERVER (Azure): ", erro);
+            });
+        });
+    } else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+        return new Promise(function (resolve, reject) {
+            var conexao = mysql.createConnection(mysqlConfig);
+            conexao.connect();
+            conexao.quedy(instrucao, function (erro, resultados){
+                conexao.end();
+                if(erro){
+                    reject(erro);
+                }
+                console.log(resultados);
+                resolve(resultados);
+            });
+            conexao.on('error', function (erro){
+                return ("ERRO NO MYSQL WORKBENCH: ", erro.sqlMessage);
+            });
+        });
+    } else{
+        return new Promise(function (resolve, reject){
+            console.log("\nO AMBIENTE (produção ou desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+            reject("AMBIENTE NÃO CONFIGURADO EM app.js")
+        });
+    }
+}
+
+module.exports = {
+    executar
+}
---
+++ src/models/alunoModels.js
@@ -0,0 +1,29 @@
+var database = require("../database/config")
+
+function autenticar(email, senha){
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT id, nome, cpf, email, fkCurso as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+//  Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, cpf, email, senha){
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+
+    // Insira exatamente a query do banco aqui, lembrando da nomenClatura exata nos valores
+    // e na ordem de inserção do dados.
+    var instrucao = `
+        INSERT INTO Aluno (nomeAluno, cpf, celular, email, senha, fkCurso) VALUES 
+        ('${nome}', '${cpf}', '${celular}', '${email}', '${senha}', '${fkCurso}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
