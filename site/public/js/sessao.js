5b1dc43f16ed27abf96ab797e214abee87c16894
Kauan-deAlmeida <kauan.almeida@sptech.school>
2023-11-27 16:34:11 -0300

    Implementado o select do nome para tela da dashboard, assim quando o usuário fazer login irá entrar na conta dele

--- site/public/js/sessao.js
+++ site/public/js/sessao.js
@@ -2,13 +2,13 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-    var cpf = sessionStorage.CPF_USUARIO;
+    var ra = sessionStorage.ID_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
-    var b_cpf = document.getElementById("b_cpf");
+    var nome_usuario_span = document.getElementById("nome_usuario_span");
+    var ra_usuario_span = document.getElementById("ra_usuario_span");
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
-        b_cpf.innerHTML = cpf;
+        nome_usuario_span.innerHTML = nome;
+        ra_usuario_span.innerHTML = ra;
     } else {
         window.location = "../login.html";
     }
